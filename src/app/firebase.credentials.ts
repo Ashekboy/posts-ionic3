c93ae3d9a903093a6a9241f4f199c5b41a214c24
Wellington <well@bigfishmedia.com.br>
2017-08-09 13:22:27 -0300

    Omitindo Configurações

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CREDENTIALS = {
-    apiKey: "AIzaSyCBPBLRNBF8i2VJPmUjAeacQYyNTu_PhiE",
-    authDomain: "posts-8fc46.firebaseapp.com",
-    databaseURL: "https://posts-8fc46.firebaseio.com",
-    projectId: "posts-8fc46",
-    storageBucket: "posts-8fc46.appspot.com",
-    messagingSenderId: "796690623792"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
